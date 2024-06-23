c9a662deb6f927f385de0c9638b9f5a9a7a6c4e7
Shana Sherfudheen <76563587+shanasherfudheen@users.noreply.github.com>
2024-06-23 06:48:16 +0530

    Add files via upload

---
+++ netflix/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import {originals, action, comedy, horror} from './urls'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title = 'Netflix Orignals'/>
+      <RowPost url={action} title = 'Action' isSmall/>
+      <RowPost url={comedy} title = 'Comedy' isSmall/>
+      <RowPost url={horror} title = 'Horror' isSmall/>
+    </div>
+  );
+}
+
+export default App;
